280e7a3679e4b07ba68e327cbd4133a27a5d4e30
Ubuntu <ubuntu@ip-172-31-9-141.ap-south-1.compute.internal>
2020-05-24 05:49:41 +0000

    kubernetes-config-scripts

---
+++ docker.sh
@@ -0,0 +1,6 @@
+sudo apt-get update  ## Update the apt-get
+
+sudo apt-get install docker.io -y    ## Installing Docker Engine or Container Runtime
+
+sudo systemctl start docker   # Start the Docker
+
